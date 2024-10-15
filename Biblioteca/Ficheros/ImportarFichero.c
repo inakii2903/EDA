3f64ec533afff17d75f6ba5a05c5962c14532c31
Manuu1301 <manueljg1301@gmail.com>
2024-10-15 23:55:58 +0200

    Inicio función ImportarFichero.c
    No tengo ni puta idea de ncurses, asiq lo he dejado con lo básico que es leer el nombre del fichero, abrirlo y separlo en puntos y comas (;),

--- Biblioteca/Ficheros/ImportarFichero.c
+++ Biblioteca/Ficheros/ImportarFichero.c
@@ -18,4 +18,101 @@ void ImportarFichero(LIBRO **Fichas,WINDOW *Wfichero,bool sumar)
    
     // Código del alumno
     
+    // Variables
+    FILE *fichero;
+    char nombreFichero[80];
+    char linea[200];
+    char *token;
+    int numLibros = 0;
+    int numLibrosFichero = 0;
+    LIBRO *aux;
+    bool error = false;
+
+
+    // Crear la ventana si no está creada
+    if (Wfichero == NULL) {
+        Wfichero = newwin(10, 40, 5, 5);
+        box(Wfichero, 0, 0);
+        wrefresh(Wfichero);
+    }
+
+    // Capturar el nombre del fichero desde la ventana
+    mvwprintw(Wfichero, 1, 1, "Ingrese el nombre del fichero: ");
+    wgetstr(Wfichero, nombreFichero);
+
+    // Abrir el fichero
+    fichero = fopen(nombreFichero, "r");
+    if (fichero == NULL) {
+        mvwprintw(Wfichero, 2, 1, "Error al abrir el fichero.");
+        wrefresh(Wfichero);
+        endwin();
+        return;
+    }
+
+
+    // Leemos los libros del fichero
+
+    while (fgets(linea,200,fichero) != NULL)
+    {
+        // Separamos los campos del libro
+        token = strtok(linea,";");
+        if (token == NULL)
+        {
+            error = true;
+            break;
+        }
+        strcpy((*Fichas)[numLibros].Titulo,token);
+        token = strtok(NULL,";");
+        if (token == NULL)
+        {
+            error = true;
+            break;
+        }
+        strcpy((*Fichas)[numLibros].ApellAutor,token);
+        token = strtok(NULL,";");
+        if (token == NULL)
+        {
+            error = true;
+            break;
+        }
+        strcpy((*Fichas)[numLibros].NomAutor,token);
+        token = strtok(NULL,";");
+        if (token == NULL)
+        {
+            error = true;
+            break;
+        }
+        strcpy((*Fichas)[numLibros].Genero,token);
+        token = strtok(NULL,";");
+
+        if (token == NULL)
+        {
+            error = true;
+            break;
+        }
+        strcpy((*Fichas)[numLibros].Editorial,token);
+        token = strtok(NULL,";");
+        if (token == NULL)
+        {
+            error = true;
+            break;
+        }
+        strcpy((*Fichas)[numLibros].Coleccion,token);
+
+        // Comprobamos si el libro tiene título y autor
+        if ((*Fichas)[numLibros].Titulo[0] == '\0' || (*Fichas)[numLibros].ApellAutor[0] == '\0' || (*Fichas)[numLibros].NomAutor[0] == '\0')
+        {
+            error = true;
+            break;
+        }
+
+        
+        numLibros++;
+    }
+
+    // Cerrar el fichero
+    fclose(fichero);
+
+
+    
 }
